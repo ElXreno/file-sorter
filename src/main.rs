e8929439770dd9cfe40d7303673301a37f72b5a6
ElXreno <elxreno@gmail.com>
2020-04-03 14:13:47 +0300

    Extracted arg matching to utils::get_arg_matches

--- src/main.rs
+++ src/main.rs
@@ -4,55 +4,13 @@ extern crate chrono;
 
 use chrono::prelude::*;
 use chrono::DateTime;
-use clap::{App, AppSettings, Arg, SubCommand};
 use std::path::PathBuf;
 
 mod settings;
 mod utils;
 
 fn main() {
-    let matches = App::new(crate_name!())
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .setting(AppSettings::ArgRequiredElseHelp)
-        .subcommand(
-            SubCommand::with_name("init")
-                .about("(Re)Initialize configuration file")
-                .arg(
-                    Arg::with_name("source_dir")
-                        .help("Source directory")
-                        .index(1)
-                        .required(true)
-                        .value_name("SOURCE"),
-                )
-                .arg(
-                    Arg::with_name("destination_dir")
-                        .help("Destination directory")
-                        .index(2)
-                        .required(true)
-                        .value_name("DESTINATION"),
-                )
-                .arg(
-                    Arg::with_name("use_date_pattern")
-                        .short("d")
-                        .long("use-date-pattern")
-                        .help("Use date pattern")
-                        .takes_value(false),
-                )
-                .arg(
-                    Arg::with_name("date_pattern")
-                        .short("p")
-                        .long("date-pattern")
-                        .help("Date subdir pattern")
-                        .takes_value(true)
-                        .default_value("%Y-%m-%d"), // 2020-01-01
-                ),
-        )
-        .subcommand(SubCommand::with_name("sort").about(
-            "Sorting source directory to destination (config file should be initialized first!)",
-        ))
-        .get_matches();
+    let matches = utils::get_arg_matches();
 
     match matches.subcommand_name() {
         Some("init") => {
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,50 @@
+use clap::{App, AppSettings, Arg, ArgMatches, SubCommand};
 use std::path::PathBuf;
+
+pub fn get_arg_matches() -> ArgMatches<'static> {
+    App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("init")
+                .about("(Re)Initialize configuration file")
+                .arg(
+                    Arg::with_name("source_dir")
+                        .help("Source directory")
+                        .index(1)
+                        .required(true)
+                        .value_name("SOURCE"),
+                )
+                .arg(
+                    Arg::with_name("destination_dir")
+                        .help("Destination directory")
+                        .index(2)
+                        .required(true)
+                        .value_name("DESTINATION"),
+                )
+                .arg(
+                    Arg::with_name("use_date_pattern")
+                        .short("d")
+                        .long("use-date-pattern")
+                        .help("Use date pattern")
+                        .takes_value(false),
+                )
+                .arg(
+                    Arg::with_name("date_pattern")
+                        .short("p")
+                        .long("date-pattern")
+                        .help("Date subdir pattern")
+                        .takes_value(true)
+                        .default_value("%Y-%m-%d"), // 2020-01-01
+                ),
+        )
+        .subcommand(SubCommand::with_name("sort").about(
+            "Sorting source directory to destination (config file should be initialized first!)",
+        ))
+        .get_matches()
+}
 
 pub fn create_dir(path: &PathBuf) {
     if !path.exists() {
